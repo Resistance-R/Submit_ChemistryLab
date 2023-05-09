1d44ae22017a69e87345e838d62912fe65cb107d
Resistance-R <125963196+Resistance-R@users.noreply.github.com>
2023-05-10 00:16:07 +0900

    first
    ssssibaaaaaal

---
+++ Assets/Scripts/AtomController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AtomController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject H_Image, He_Image, Li_Image, Be_Image, B_Image, C_Image, N_Image, O_Image, F_Image, Ne_Image, Na_Image, Mg_Image, Fe_Image;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
